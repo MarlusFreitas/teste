b3c6f9184069f1a3b1377d8f0f627c5a854c053a
MarlusFreitas <marlus_fox@hotmail.com>
2025-01-27 18:24:11 -0300

    Atualizada exclusão

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -33,7 +33,7 @@ public class Product {
         StringBuilder sb = new StringBuilder();
         sb.append(name);
         sb.append(", $").append(String.format("%.2f", price));
-        sb.append(", Quantity: ").append(Oqu);
+        sb.append(", Quantity: ").append(SOBRE - APAGADO);
         return sb.toString();
     }
 
